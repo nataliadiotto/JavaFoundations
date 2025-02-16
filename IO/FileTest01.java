14ccacfca012f9ab28ba11e51bcabc1297034793
Natália Diotto <diottonatalia@gmail.com>
2025-02-16 12:50:27 -0300

    Main File methods

--- IO/FileTest01.java
+++ IO/FileTest01.java
@@ -2,6 +2,9 @@ package IO;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
 
 public class FileTest01 {
 
@@ -10,6 +13,15 @@ public class FileTest01 {
         File file = new File("file.txt");
         try {
             boolean isCreated = file.createNewFile();
+            System.out.println(isCreated);
+            System.out.println("Path: " + file.getPath());
+            System.out.println("Absolute Path: " + file.getAbsolutePath());
+            System.out.println("Is directory? " + file.isDirectory());
+            System.out.println("Is file? " + file.isFile());
+            System.out.println("Is hidden? " + file.isHidden());
+            System.out.println("Is directory? " + file.isDirectory());
+            System.out.println("Last modified: " + Instant.ofEpochMilli(file.lastModified())
+                    .atZone(ZoneId.systemDefault()).toLocalDateTime());
 
         } catch (IOException e) {
             e.printStackTrace();
