341117256acdd65e0b990d4f4af7c5687e1f7e49
Natália Diotto <diottonatalia@gmail.com>
2025-02-17 09:48:28 -0300

    Copying files with Path

--- NIO/PathTest02.java
+++ NIO/PathTest02.java
@@ -4,17 +4,38 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class PathTest02 {
     public static void main(String[] args) throws IOException {
-        Path filePath = Paths.get("file02");
+        Path folderPath = Paths.get("folder");
 
-        if (Files.notExists(filePath)) {
-            Path fileDirectory = Files.createDirectory(filePath);
+        if (Files.notExists(folderPath)) {
+            Path fileDirectory = Files.createDirectory(folderPath);
         }
 
         //mutliplos diretorios, criando subpastas
-        Path subFilePath = Paths.get("file03/subfile/subsubfile");
-        Path subFileDirectory = Files.createDirectories(subFilePath);
+        Path subFolderPath = Paths.get("folder/subfolder/subsubfolder");
+        Path subFileDirectory = Files.createDirectories(subFolderPath);
+
+        //criando arquivos
+        Path filePath = Paths.get(subFolderPath.toString(), "file.txt");
+        if (Files.notExists(filePath)) {
+            Path filePathCreated = Files.createFile(filePath);
+        }
+
+        Path filePath02 = Paths.get("/Users/Natalia/JavaFoundations/folder/subfolder",
+                "file02.txt");
+
+        if (Files.notExists(filePath02)) {
+            Path subFilePathCreated = Files.createFile(filePath02);
+        }
+
+        //copiando arquivos
+        Path source = filePath;
+        Path target = Paths.get(filePath.getParent().toString(), "renamed_file.txt");
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+
+
     }
 }
