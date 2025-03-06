ebcfd203a1a64cb4aa1cad5dbd69521f2f0c1e46
Natália Diotto <diottonatalia@gmail.com>
2025-03-06 11:03:22 -0300

    Operacoes terminais em Stream

--- Streams/domain/LightNovel.java
+++ Streams/domain/LightNovel.java
@@ -1,5 +1,7 @@
 package Streams.domain;
 
+import java.util.Objects;
+
 public class LightNovel {
 
     private String title;
@@ -24,5 +26,18 @@ public class LightNovel {
                 "title='" + title + '\'' +
                 ", price=" + price +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LightNovel that = (LightNovel) o;
+        return Objects.equals(title, that.title) && Objects.equals(price, that.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, price);
     }
 }
